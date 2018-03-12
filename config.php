5d836dfa3c7bc9b6d539a805aee65341f092e243
Prasanga Dhakal <prasanga.dhakal@gmail.com>
2018-03-12 03:44:22 -0230

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $dbhost = 'mysql.cs.mun.ca';
-$dbuser = 'cs3715w17_aw7464';
-$dbpass = '$ZRc#33b';
+$dbuser = '';
+$dbpass = '';
 
 $db= new mysqli($dbhost,$dbuser,$dbpass,$dbuser);
 if($db->connect_errno>0){
